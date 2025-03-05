5325d50c3424c5a977db3b24c7a0e12915810c00
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-05 13:50:37 +0530

    str

--- 2071/B.py
+++ 2071/B.py
@@ -1,9 +1,5 @@
 t=int(input())
 for _ in range(t):
     n=int(input())
-    if(typeof(((n)*(n+1)//2)**0.5))==int)):
-        print(-1)
-    else:
-        # n^2-n=2*k^2
-        res=[i for i in range(n)]
-        print(res)
+    res=[i for i in range(n)]
+    print(res)
