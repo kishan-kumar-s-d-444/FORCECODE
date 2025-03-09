fb810bce544b4213354f2b40f8917d763f8f2560
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-09 07:22:29 +0530

    2069A

--- 2069/A.py
+++ 2069/A.py
@@ -7,11 +7,19 @@ for _ in range(t):
         if(b[i]==1):
             arr[i]=1
             arr[i+1]=1
-            arr[i+2]=1 
-    print(arr)
+            arr[i+2]=1
+    num=2 
+    for i in range(n):
+        if(arr[i]==-1):
+            arr[i]=num
+            num+=1
+    # print(arr)
     res="YES"
     for i in range(n-2):
         if(b[i]==0 and arr[i]==arr[i+1] and arr[i+1]==arr[i+2]):
+            res="NO"
+            break
+        if(b[i]==1 and (arr[i]!=arr[i+1] or arr[i+1]!=arr[i+2])):
             res="NO"
             break
     print(res)
