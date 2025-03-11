8422b9cf6a664fedb560b65069636ae51193098d
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-11 06:31:41 +0530

    2069A

--- 2069/A.py
+++ 2069/A.py
@@ -7,11 +7,11 @@ for _ in range(t):
     for i in range(n-2):
         if(b[i]==1):
             if(arr[i]!=-1 or arr[i+1]!=-1 or arr[i+2]!=-1):
-                num+=1
                 arr[i]=num
                 arr[i+1]=num
                 arr[i+2]=num
             else:
+                num+=1
                 arr[i]=num
                 arr[i+1]=num
                 arr[i+2]=num
@@ -19,7 +19,7 @@ for _ in range(t):
         if(arr[i]==-1):
             num+=1
             arr[i]=num
-    # print(arr)
+    print(arr)
     res="YES"
     for i in range(n-2):
         if(b[i]==0 and arr[i]==arr[i+1] and arr[i+1]==arr[i+2]):
