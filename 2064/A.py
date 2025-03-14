3b6f22a8c63d2d925e43fdfac5547a239c04c103
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-14 18:58:27 +0530

    2064A

--- 2064/A.py
+++ 2064/A.py
@@ -2,4 +2,12 @@ t=int(input())
 for _ in range(t):
     n=int(input())
     s=input()
-    print(s)
+    ans=0
+    flag=True
+    for i in range(n-1):
+        if(flag and s[i]=='1'):
+            ans+=1
+            flag=False
+        if(s[i]=='0'):
+            flag=True
+    print(ans)
