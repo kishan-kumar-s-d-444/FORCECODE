461e28e6bd2b7e14b8af889e24b5e3dfa6e4509e
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-20 18:07:53 +0530

    2075B

--- 2075/B.py
+++ 2075/B.py
@@ -13,9 +13,40 @@ for _ in range(t):
         done[ind]=1
         res=res+(-ele)
         k-=1
-    
-    for i in range(n-1,-1,-1):
-        if(done[i]==0):
-            res+=a[i]
-            break
+    if(done[0]==0 and done[n-1]==0):
+        res+=max(a[0],a[n-1])
+    elif(done[0]==0):
+        i=n-1
+        maxi=float('-inf')
+        while(i>=0):
+            rv=float('-inf')
+            while(i>=0 and done[i]==0):
+                rv=max(rv,a[i])
+                i-=1
+            if(i<0):
+                rv=a[0]
+            else:
+                i-=1
+            max=max(maxi,rv)
+        res+=maxi
+    elif(done[n-1]==0):
+        i=0
+        maxi=float('-inf')
+        while(i<n):
+            rv=float('-inf')
+            while(i<n and done[i]==0):
+                rv=max(rv,a[i])
+                i+=1
+            if(i==n):
+                rv=a[0]
+            else:
+                i-=1
+            max=max(maxi,rv)
+        res+=maxi
+    else:
+        maxi=float('-inf')
+        for i in range(n):
+            if(done[i]==0):
+                maxi=max(maxi,a[i])
+        res+=maxi
     print(res)
