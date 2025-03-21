40ec26d6c6658f229b42ae2d5c20dfe3f95bbd14
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-21 07:17:12 +0530

    2075B

--- 2075/B.py
+++ 2075/B.py
@@ -7,46 +7,11 @@ for _ in range(t):
     for i in range(n):
         heapq.heappush(heap,[-a[i],i])
     done=[0]*n
+    nk=k+1
     res=0
-    while(heap and k):
+    while(heap and nk):
         ele,ind=heapq.heappop(heap)
         done[ind]=1
         res=res+(-ele)
-        k-=1
-    if(done[0]==0 and done[n-1]==0):
-        res+=max(a[0],a[n-1])
-    elif(done[0]==0):
-        i=n-1
-        maxi=float('-inf')
-        while(i>=0):
-            rv=float('-inf')
-            while(i>=0 and done[i]==0):
-                rv=max(rv,a[i])
-                i-=1
-            if(i<0):
-                rv=a[0]
-            else:
-                i-=1
-            max=max(maxi,rv)
-        res+=maxi
-    elif(done[n-1]==0):
-        i=0
-        maxi=float('-inf')
-        while(i<n):
-            rv=float('-inf')
-            while(i<n and done[i]==0):
-                rv=max(rv,a[i])
-                i+=1
-            if(i==n):
-                rv=a[0]
-            else:
-                i-=1
-            max=max(maxi,rv)
-        res+=maxi
-    else:
-        maxi=float('-inf')
-        for i in range(n):
-            if(done[i]==0):
-                maxi=max(maxi,a[i])
-        res+=maxi
+        nk-=1
     print(res)
