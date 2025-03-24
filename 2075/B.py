5e8c00db326cb7a3a49fc009da1ff93c906d5fc8
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-24 07:44:52 +0530

    2075B - Not Done

--- 2075/B.py
+++ 2075/B.py
@@ -9,13 +9,29 @@ for _ in range(t):
     done=[0]*n
     nk=k
     res=0
-    while(heap and nk):
-        ele,ind=heapq.heappop(heap)
-        done[ind]=1
-        res=res+(-ele)
-        nk-=1
     if(k==1):
-        ele,ind=heapq.heappop(heap)
-        done[ind]=1
-        res=res+(-ele)
+        while(heap):
+            ele,ind=heapq.heappop(heap)
+            if(ind!=0 and ind!=n-1):
+                res+=a[ind]
+                break
+        res+=max(a[0],a[n-1])
+    else:
+        temp=[]
+        prev=0
+        while(heap and k):
+            ele,ind=heapq.heappop(heap)
+            done[ind]=1
+            temp.append([prev,ind])
+            prev=ind
+            res+=a[ind]
+            k-=1
+        temp.append([prev,n])
+        maxi=float('-inf')
+        # print(temp,"k")
+        for l,r in temp:
+            if(r-l>1):
+                # print(a[l+1:r],"i")
+                maxi=max(maxi,max(a[l+1:r]))
+        res+=maxi
     print(res)
