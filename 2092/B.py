56e270844bdad24ecd3990ff98aa61ef5f47e1e7
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-03-30 11:27:36 +0530

    2092B

---
+++ 2092/B.py
@@ -0,0 +1,41 @@
+import math
+t=int(input())
+for _ in range(t):
+    flag=True
+    n=int(input())
+    a=input()
+    b=input()
+    z1=0
+    for i in range(n):
+        if(i%2==0):
+            if(a[i]=='0'):
+                z1+=1
+        else:
+            if(b[i]=='0'):
+                z1+=1
+    if(z1>=math.ceil(n/2)):
+        flag=False
+    if(flag):
+        print("No")
+    else:
+        flag=True
+        z2=0
+        for i in range(n):
+            if(i%2==0):
+                if(b[i]=='0'):
+                    z2+=1
+            else:
+                if(a[i]=='0'):
+                    z2+=1
+        if(z2>=math.ceil(n/2)):
+            flag=False
+        if(flag):
+            print("No")
+        else:
+            print("Yes")
+        
+        
+
+    
+
+    
