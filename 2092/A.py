f02e4f3a27a083f1a3b4b466a44d9bcafc1843eb
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-04-21 07:42:25 +0530

    2092A

--- 2092/A.py
+++ 2092/A.py
@@ -4,6 +4,6 @@ for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
     low,high=min(a),max(a)
-    diff=high-low-1
+    diff=high-low-1-1
     res=math.gcd(low+diff,high+diff)
     print(res)
