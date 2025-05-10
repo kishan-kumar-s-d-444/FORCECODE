7249fba0f1cb4ca37f47d91d474ea65aff1ce8ae
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-05-10 08:25:23 +0530

    2107A

--- 2107/A.py
+++ 2107/A.py
@@ -10,7 +10,26 @@ for _ in range(t):
         else:
             even.append(a[i])
     if(not odd):
-        print("No")
+        flag=True
+        for i in range(n-1):
+            if(even[i]!=even[i+1]):
+                flag=False
+                break
+        if(flag):
+            print("No")
+        else:
+            temp=False
+            print("Yes")
+            for i in range(n):
+                if(a[i]==2):
+                    print(2,end=" ")
+                else:
+                    if(temp):
+                        print(2,end=" ")
+                    else:
+                        print(1,end=" ")
+                        temp=True
+            print(" ")
     elif(not even):
         flag=True
         for i in range(n-1):
