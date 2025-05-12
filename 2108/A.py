e4ecf5fa686a1feb1cc0221508e332e1d5d943dd
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-05-12 09:17:12 +0530

    2108A

--- 2108/A.py
+++ 2108/A.py
@@ -1,13 +1,10 @@
 t=int(input())
 for _ in range(t):
     n=int(input())
-    res=0
-    for i in range(1,n+1):
-        cur=0
-        for j in range(1,n+1):
-            cur+=abs(i-j)
-        res+=cur*(n-1)
-    if(n==1):
-        print(1)
-    else:
-        print(res)
+    ans=0
+    q1=0
+    ind=1
+    for i in range(n,0,-1):
+        q1+=abs(i-ind)
+        ind+=1
+    print(q1//2+1)
