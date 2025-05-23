1348f813a9cc4f118bb32f501088bd22d702c8c9
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-05-23 09:24:07 +0530

    2057B

---
+++ 2057/B.py
@@ -0,0 +1,24 @@
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    dic={}
+    q1,q2=-1,-1
+    for i in a:
+        dic[i]=dic.get(i,0)+1
+        if(dic[i]>q2):
+            q1=i
+            q2=dic[i]
+    dic_n= {key: val for key, val in sorted(dict.items(), key = lambda ele: ele[1], reverse = False)}
+    dic_q={}
+    for nm,cn in dic_n.items():
+        if(k>0):
+            if(cn>=k):
+                dic_q[q1]=dic_q.get(q1,0)+cn
+                cn-=k
+            else:
+                dic_q[q1]=dic_q.get(q1,0)+k
+                dic_q[nm]=dic_q.get(nm,0)+(cn-k)
+        else:
+            dic_q[nm]=dic_q.get(nm,0)+cn
+    print(len(dic_q))
