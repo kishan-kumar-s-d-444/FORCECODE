c6287926762f6d0ff9aa604c5c426b425d703a0b
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-05-24 11:00:39 +0530

    2057B

--- 2057/B.py
+++ 2057/B.py
@@ -9,16 +9,20 @@ for _ in range(t):
         if(dic[i]>q2):
             q1=i
             q2=dic[i]
-    dic_n= {key: val for key, val in sorted(dict.items(), key = lambda ele: ele[1], reverse = False)}
+    dic_n= {key: val for key, val in sorted(dic.items(), key = lambda ele: ele[1], reverse = False)}
     dic_q={}
+    # print(dic_n)
     for nm,cn in dic_n.items():
         if(k>0):
-            if(cn>=k):
-                dic_q[q1]=dic_q.get(q1,0)+cn
-                cn-=k
-            else:
-                dic_q[q1]=dic_q.get(q1,0)+k
-                dic_q[nm]=dic_q.get(nm,0)+(cn-k)
+            if(nm!=q1):
+                if(k>=cn):
+                    dic_q[q1]=dic_q.get(q1,0)+cn
+                    k-=cn
+                else:
+                    dic_q[q1]=dic_q.get(q1,0)+k
+                    dic_q[nm]=dic_q.get(nm,0)+(cn-k)
+                    k=0
         else:
             dic_q[nm]=dic_q.get(nm,0)+cn
+    # print(dic_q)
     print(len(dic_q))
