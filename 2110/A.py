7bf5d0e3fb7447af7e86d3f0a69b412b9cdbdbef
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-05-26 10:13:53 +0530

    2110A

---
+++ 2110/A.py
@@ -0,0 +1,11 @@
+from collections import Counter
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    count=Counter(a)
+    a.sort()
+    if((a[0]+a[-1])%2==0):
+        print(0)
+    else:
+        print(min(count[a[0]],count[a[-1]]))
