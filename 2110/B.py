7e02b79c82149ed052d3b72c9410f93c160717b8
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-01 06:52:56 +0530

    2110B

---
+++ 2110/B.py
@@ -0,0 +1,12 @@
+t=int(input())
+for _ in range(t):
+    s=input()
+    n=len(s)
+    flag=False
+    for i in range(n//2):
+        if(s[i]==')'):
+            flag=True
+    if(flag):
+        print("NO")
+    else:
+        print("Yes")
