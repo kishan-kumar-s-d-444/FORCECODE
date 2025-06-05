dc8cb114693a8fcd562e01386eb718fe00de3d8d
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-05 05:44:12 +0530

    2110B

--- 2110/B.py
+++ 2110/B.py
@@ -4,13 +4,13 @@ for _ in range(t):
     n=len(s)
     flag=False
     for i in range(n//2):
-        if(s[i]==')'):
+        if(s[i]==')' and s[n-i-1]==')'):
             flag=True
     for i in range(n//2,n):
-        if(s[i]=='('):
+        if(s[i]=='(' and s[n-i-1]=='('):
             flag=True
     if(flag):
         print("YES")
     else:
         print("NO")
-#(()())
+#(()()) 
