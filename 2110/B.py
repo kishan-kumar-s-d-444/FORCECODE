3a16bebba7189376c3c714e0e4d9b4153146c7f6
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-06 05:27:40 +0530

    2110B

--- 2110/B.py
+++ 2110/B.py
@@ -3,12 +3,12 @@ for _ in range(t):
     s=input()
     n=len(s)
     flag=False
+    stack=[]
     for i in range(n//2):
-        if(s[i]==')' and s[n-i-1]==')'):
-            flag=True
-    for i in range(n//2,n):
-        if(s[i]=='(' and s[n-i-1]=='('):
-            flag=True
+        if(i+1!=n//2):
+            if(s[i]==')' and s[i+1]=='('):
+                flag=True
+
     if(flag):
         print("YES")
     else:
