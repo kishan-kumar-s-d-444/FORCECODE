95cb0e793d1c37e0452809c9cb22e14b52048340
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-08 07:53:37 +0530

    2104B

---
+++ 2104/B.py
@@ -0,0 +1,26 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    pre=[0]*(n)
+    maxi=0
+    for i in range(n):
+        maxi=max(maxi,a[i])
+        pre[i]=maxi
+    suf=[0]*(n)
+    sm=0
+    for i in range(n-1,-1,-1):
+        sm+=a[i]
+        suf[i]=sm    
+    res=[0]*(n)
+
+    for i in range(n-1,-1,-1):
+        if(i==n-1):
+            res[i]=pre[i]
+        else:
+            res[i]=suf[i+1]+pre[i]
+    
+    for i in range(n):
+        print(res[i],end=" ")
+    print("/n")
+        
