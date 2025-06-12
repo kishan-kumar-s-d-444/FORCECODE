774c29e79aeb5bfae8a826be2ae388499526d9b2
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-13 00:31:57 +0530

    2117C

--- 2117/C.py
+++ 2117/C.py
@@ -4,22 +4,30 @@ for _ in range(t):
     a=list(map(int,input().split()))
     ans=1
     prev={}
+    prev_cnt=0
     flag=True
     cur={a[0]:1}
-    ind=1
+    cur_cnt=1
     for i in range(1,n):
+        # print(i,"\n",prev,prev_cnt,"\n",cur,cur_cnt)
         if(flag):
-            if(a[i] in cur):
-                del cur[a[i]]
+            if(a[i] in cur and cur[a[i]]!=-1):
+                cur[a[i]]=-1
+                cur_cnt-=1
+            if(a[i] not in prev or prev[a[i]]==-1):
+                prev_cnt+=1
             prev[a[i]]=1
-            if(not cur):
+            if(cur_cnt==0):
                 flag=False
                 ans+=1
         else:
-            if(a[i] in prev):
-                del prev[a[i]]
+            if(a[i] in prev and prev[a[i]]!=-1):
+                prev[a[i]]=-1
+                prev_cnt-=1
+            if(a[i] not in cur or cur[a[i]]==-1):
+                cur_cnt+=1
             cur[a[i]]=1
-            if(not prev):
+            if(prev_cnt==0):
                 flag=True
                 ans+=1
     print(ans)
