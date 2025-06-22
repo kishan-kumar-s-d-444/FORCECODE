9bb1b7adf700b4e5ee7d09360ca9b68e3e527cd2
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-22 09:30:36 +0530

    2111C

--- 2111/C.py
+++ 2111/C.py
@@ -3,8 +3,6 @@ t=int(input())
 for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
-    # for i in range(1,n):
-    #     if(a[i]==a[0] and )
     mini=float('inf')
     for i in range(n):
         val=(i*a[i])+((n-i-1)*a[i])
