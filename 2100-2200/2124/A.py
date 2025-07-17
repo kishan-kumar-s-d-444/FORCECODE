cd72bcbfb97cc469fab96b4ed7d67b4eb673e57f
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-07-17 11:16:06 +0530

    2124A

--- 2100-2200/2124/A.py
+++ 2100-2200/2124/A.py
@@ -7,8 +7,11 @@ for _ in range(t):
         print("NO")
     else:
         print("YES")
+        res=[]
         for i in range(n):
             if(a[i]!=b[i]):
-                print(1)
-                print(a)
-                break
+                res.append(a[i])
+        print(len(res))
+        for i in range(len(res)):
+            print(res[i],end=" ")
+        print(" ")
