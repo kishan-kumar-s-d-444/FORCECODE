006aa813d75ff6cab4eac33d017d89cdaaf44a0e
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-07-19 11:57:44 +0530

    2111B

---
+++ 2100-2200/2111/B.py
@@ -0,0 +1,10 @@
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    boxes=[]
+    res=""
+    for _ in range(m):
+        w,l,h=map(int,input().split())
+        res+='0'
+    print(res)
+
