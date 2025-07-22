d20946331f3ba43be3548882c2a26882be847b47
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-07-22 14:54:54 +0530

    2126C

--- 2100-2200/2126/C.py
+++ 2100-2200/2126/C.py
@@ -1,6 +1,3 @@
-from re import L
-
-
 t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
@@ -12,24 +9,21 @@ for _ in range(t):
     q1.sort()
     for i in range(n):
         if(q1[i][1]==k-1):
-            k1=q1[i][0]
             k2=i
-    water=1
+    water=0
     x=0
     flag=False
-    cur=k1
     while(1):
         if(k2==n-1):
             flag=True
             break
-        if(cur<water):
+        cur=q1[k2][0]
+        dest=q1[k2+1][0]
+        time=abs(cur-dest)
+        if(water+time>cur):
             break
-        mom=x+abs(k1-q1[k2+1][0])
-        water+=1
-        x=mom
-        cur=q1[k2+1][0]
+        water+=time
         k2+=1
-        
     if(flag):
         print("YES")
     else:
