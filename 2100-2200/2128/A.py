5f4564e15846399fde1e06ebba41b17a06ace5eb
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-07-29 11:39:34 +0530

    2128A

--- 2100-2200/2128/A.py
+++ 2100-2200/2128/A.py
@@ -2,4 +2,22 @@ t=int(input())
 for _ in range(t):
     n,c=map(int,input().split())
     a=list(map(int,input().split()))
-    print(a)
+    a.sort()
+    cost=0
+    ind=-1
+    for i in range(n-1,-1,-1):
+        if(a[i]<c):
+            ind=i
+            break
+        else:
+            cost+=1
+    fact=1
+    left=0
+    right=ind
+    while(left<=right):
+        while(a[right]*fact>c):
+            right-=1
+            cost+=1
+        right-=1
+        fact*=2
+    print(cost)
