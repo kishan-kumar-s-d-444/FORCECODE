706828bb24915e5481e02ee62d5d558e282f1c39
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-08-06 22:20:12 +0530

    2130B

--- 2100-2200/2130/B.py
+++ 2100-2200/2130/B.py
@@ -2,7 +2,8 @@ t=int(input())
 for _ in range(t):
     n,s=map(int,input().split())
     a=list(map(int,input().split()))
-    for i in range(n):
-        for j in range(n):
-            print(a[j])
+    if(sum(a)%n==0):
+        print(-1)
+    else:
+        print([i for i in range(1,n+1)])
     
