c411c5428112d8dda15032cff121957708efb4b9
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-08-08 21:33:44 +0530

    2127A

---
+++ 2100-2200/2127/A.py
@@ -0,0 +1,24 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    flag=True
+    for i in range(n-2):
+        maxi=float('-inf')
+        mini=float('inf')
+        for j in range(i,i+3):
+            if(a[j]!=-1):
+                maxi=max(maxi,a[j])
+                mini=min(mini,a[j])
+        if(maxi==float('inf')):
+            continue
+        for j in range(max+2):
+            if(j not in a[i:i+3]):
+                mex=j
+        if(mex!=maxi-mini):
+            flag=False
+            break
+    if(flag):
+        print("yes")
+    else:
+        print("No")
