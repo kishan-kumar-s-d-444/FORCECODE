23ce5e574852ec54818cce0b123b985ce3098ec5
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-08-16 07:03:17 +0530

    2131C

--- 2100-2200/2131/C.py
+++ 2100-2200/2131/C.py
@@ -3,47 +3,22 @@ for _ in range(t):
     n,k=map(int,input().split())
     S=list(map(int,input().split()))
     T=list(map(int,input().split()))
-    dic1={}
+    flag=True
     dic2={}
-    for i in range(n):
-        q1=S[i]%k
-        dic1[q1]=dic1.get(q1,0)+1
     for i in range(n):
         q1=T[i]%k
         dic2[q1]=dic2.get(q1,0)+1
-    # print(dic1)
-    # print(dic2)
-
-    flag=True
-    for nm,cnt in dic1.items():
-        key1=abs(nm-k)
-        key2=nm
-        if(key1 in dic2):
-            if(dic2[key1]>=dic1[nm]):
-                dic2[key1]=dic2[key1]-dic1[nm]
-                dic1[nm]=0
-            else:
-                dic1[nm]=dic1[nm]-dic2[key1]
-                dic2[key1]=0
-        if(dic1[nm]>0):
-            if(key2 in dic2 and dic2[key2]>=dic1[nm]):
-                dic2[key2]=dic2[key2]-dic1[nm]
-                dic1[nm]=0
-            else:
-                flag=False
-                break
-        if(dic1[nm]>0):
+    for i in range(n):
+        q1=S[i]%k
+        key1=abs(q1-k)
+        key2=q1
+        if(key1 in dic2 and dic2[key1]>0):
+            dic2[key1]-=1
+        elif(key2 in dic2 and dic2[key2]>0):
+            dic2[key2]-=1
+        else:
             flag=False
-            break
-        # print("HI",dic1,dic2)
-    for v in dic2.values():
-        if(v>0):
-            flag=False
-            break
-    for v in dic1.values():
-        if(v>0):
-            flag=False
-            break
+            break     
     if(flag):
         print("YES")
     else:
--- 2100-2200/2131/tempCodeRunnerFile.py
+++ 2100-2200/2131/tempCodeRunnerFile.py
@@ -3,23 +3,22 @@ for _ in range(t):
     n,k=map(int,input().split())
     S=list(map(int,input().split()))
     T=list(map(int,input().split()))
-    dic1={}
+    flag=True
     dic2={}
     for i in range(n):
-        q1=S[i]//k
-        q2=S[i]-(q1*k)
-        dic1[q2]=dic1.get(q2,0)+1
-        r1=T[i]//k
-        r2=T[i]-(r1*k)
-        dic2[r2]=dic2.get(r2,0)+1
-    flag=True
-    for nm,cn in dic1.items():
-        key=abs(nm-k)
-        if(key in dic2 and dic2[key]==cn):
-            continue
+        q1=T[i]%k
+        dic2[q1]=dic2.get(q1,0)+1
+    for i in range(n):
+        q1=S[i]%k
+        key1=abs(q1-k)
+        key2=q1
+        if(key1 in dic2 and dic2[key1]>0):
+            dic2[key1]-=1
+        elif(key2 in dic2 and dic2[key1]>0):
+            dic2[key2]-=1
         else:
             flag=False
-            break
+            break     
     if(flag):
         print("YES")
     else:
