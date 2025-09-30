bba3f5276c3e21256f7dbea7f5aad887d148ee15
Kishan Kumar S D <kishankumarsd444@example.com>
2025-09-30 14:31:58 +0530

    2134B

--- 2100-2200/2134/B.py
+++ 2100-2200/2134/B.py
@@ -1,2 +1,14 @@
 t=int(input())
 for _ in range(t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    res=[]
+    for i in range(n):
+        res[i]=a[i]+(a[i]%(k+1))*k
+    n=len(res)
+    for i in range(n):
+        if(i==n-1):
+            print(res[i])
+        else:
+            print(res[i],end=" ")
+    
