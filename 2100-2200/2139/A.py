6c5cbb1201c30f5d2e21edaf7333651c6ece026f
Kishan Kumar S D <kishankumarsd444@example.com>
2025-10-18 12:31:58 +0530

    2139A

---
+++ 2100-2200/2139/A.py
@@ -0,0 +1,11 @@
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    if(a<b):
+        a,b=b,a
+    if(a==b):
+        print(0)
+    elif(a%b==0):
+        print(1)
+    else:
+        print(2)
