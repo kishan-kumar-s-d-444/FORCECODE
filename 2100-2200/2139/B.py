5ac85de4ed616c82eebd29c725b68ce04d1efddb
Kishan Kumar S D <kishankumarsd444@example.com>
2025-10-20 10:56:15 +0530

    2139B

---
+++ 2100-2200/2139/B.py
@@ -0,0 +1,13 @@
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    a=list(map(int,input().split()))
+    a.sort()
+    ans=0
+    for i in range(n-1,-1,-1):
+        ans+=(a[i]*m)
+        m-=1
+        if(m<=0):
+            break
+    print(ans)
+
