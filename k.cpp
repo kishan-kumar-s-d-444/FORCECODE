f2c46c88a0e6e017c2f71feac0e4ec165bcf9ac9
Kishan Kumar S D <kishankumarsd444@gmail.com>
2025-10-21 08:48:40 +0530

    2#

--- k.cpp
+++ k.cpp
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 int main() {
     cout << "Try programiz.pro";
     return 0;
 }
-=======
-t=int(input())
-for _ in range(t):
-    n,k=map(int,input().split())
-    a=list(map(int,input().split()))
-    s=input()
-
->>>>>>> 17b85890a04aaba1cfe144218d172d6fc385b18a
+// git branch -m main
+// git add .
+// git push origin main
