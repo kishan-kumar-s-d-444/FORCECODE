4568b0d912ec8bbd066d3d6282d7fdbfca6dcd7b
Kishan Kumar S D <kishankumarsd444@gmail.com>
2025-10-25 09:30:21 +0530

    2148C

---
+++ 2100-2200/2148/C.py
@@ -0,0 +1,21 @@
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    dic={}
+    for _ in range(n):
+        a,b=map(int,input().split())
+        dic[a]=b
+    cur=0
+    at=0
+    score=0
+    while(1):
+        if(cur in dic):
+            if(dic[cur]!=at):
+                at=dic[cur]
+                score-=1
+        if(cur==m):
+            break
+        at=at^1
+        score+=1
+        cur+=1
+    print(score)
